348da3ab69ce8aa171d80a99f277fa6bfc9b48e0
Himanshu Yadav <hopeisalive2233@hotmail.com>
2024-07-20 12:17:03 +0530

    created address edit and view form

---
+++ src/component/AddressEditForm.tsx
@@ -0,0 +1,92 @@
+import { FieldValues, useForm } from "react-hook-form";
+import Input from "./Input";
+import Select from "./Select";
+import Button from "./Button";
+import { login as storeLogin } from "../features/authSlice";
+import { Iuser } from "../types/types";
+import { updateProfile } from "../api/userApi";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+type editFormType = {
+  userData: Iuser;
+  setEditToggle: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const AddressEditForm = ({ userData, setEditToggle }: editFormType) => {
+  const dispatch = useDispatch();
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      address: userData?.address || "",
+      state: userData?.state || "",
+      country: userData?.country || "",
+      pincode: userData?.pincode || "",
+    },
+  });
+  const signup = async (data: FieldValues) => {
+    try {
+      const updateResponse = await updateProfile(data);
+
+      if (updateResponse.data.success) {
+        const userData = updateResponse.data.data;
+        dispatch(storeLogin({ userData }));
+
+        setEditToggle(false);
+      } else {
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // console.log("use date    ddddd", new Date(userData?.dob));
+  return (
+    <div className="w-1/2 flex  justify-center  mx-auto">
+      <form onSubmit={handleSubmit(signup)}>
+        <div>
+          <Input
+            label="Address"
+            className="rounded-lg py-2 px-3 w-full border-2"
+            placeholder="Enter your House No ,Locality ,city"
+            {...register("address", {
+              required: true,
+            })}
+          />
+
+          <Input
+            label="State"
+            type="text"
+            className="rounded-lg py-2 px-3 w-full mt-2 mb-2 border-2"
+            placeholder="Enter your State "
+            {...register("state", {
+              required: true,
+            })}
+          ></Input>
+
+          <Select
+            options={["india"]}
+            label="Country"
+            className=" "
+            {...register("country", { required: true })}
+          />
+          <Input
+            label="Pincode"
+            type="Number"
+            className="rounded-lg py-2 px-3 w-full mt-2 mb-2 border-2"
+            placeholder="Enter your pincode "
+            {...register("pincode", {
+              required: true,
+            })}
+          ></Input>
+
+          <Button
+            type="submit"
+            className="bg-sky-500 w-full py-2  mt-4 rounded-lg text-white"
+          >
+            Save
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddressEditForm;
--- src/pages/Address.tsx
+++ src/pages/Address.tsx
@@ -1,14 +1,101 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaRegAddressBook, FaRegUser } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { Iuser } from "../types/types";
+import { RootState } from "../store/Store";
+import { DiVim } from "react-icons/di";
+import { FieldValues, useForm } from "react-hook-form";
+import Button from "../component/Button";
+import Input from "../component/Input";
+import Select from "../component/Select";
+import ProfileEditForm from "../component/ProfileEditForm";
+import AddressEditForm from "../component/AddressEditForm";
 
 function Address() {
+  let userData = useSelector(
+    (state: RootState) => state.auth.userData
+  ) as Iuser | null;
+  const [isLoading, setIsLoading] = useState(true);
+  const [editToggle, setEditToggle] = useState(false);
+  useEffect(() => {
+    if (userData !== undefined) {
+      setIsLoading(false);
+    }
+  }, [userData]);
+  const dispatch = useDispatch();
+
+  const displayData = (
+    <div className="    flex flex-col items-center ">
+      <div className=" space-y-4 border-gray-200  border-2 p-9 ">
+        <div className=" ">
+          <span className="pr-6">Address</span> <span>:</span>{" "}
+          <span
+            className={
+              userData?.state === undefined ? "text-red-500 pl-5" : "pl-5"
+            }
+          >
+            {userData?.address || "Not Added*"}
+          </span>
+        </div>
+        <div className=" ">
+          <span className=" pr-11">State</span> <span>:</span>{" "}
+          <span
+            className={
+              userData?.state === undefined ? "text-red-500 pl-5" : "pl-5"
+            }
+          >
+            {userData?.state || "Not Added*"}
+          </span>
+        </div>
+
+        <div className="">
+          <span className="pr-6">Country</span> <span>:</span>{" "}
+          <span
+            className={
+              userData?.country === undefined ? "text-red-500 pl-5" : "pl-5"
+            }
+          >
+            {" "}
+            {userData?.country || "Not Added*"}
+          </span>
+        </div>
+        <div className="">
+          <span className=" pr-6">pincode</span> <span>:</span>
+          <span
+            className={
+              userData?.pincode === undefined ? "text-red-500 pl-6" : "pl-6"
+            }
+          >
+            {userData?.pincode || "Not Added*"}
+          </span>
+        </div>
+
+        <Button
+          type="submit"
+          className="bg-sky-500 w-full py-2  mt-2 rounded-lg text-white"
+          onClick={() => setEditToggle(true)}
+        >
+          Edit
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
-    <div>
-      <div>address</div>
-      <div>address line</div>
-      <div>pincode</div>
-      <div>state</div>
-      <div></div>
-      <div>country</div>
+    <div className="space-y-5">
+      <div className="flex space-x-2 justify-center pt-6">
+        <FaRegAddressBook size={30} />
+        <div className="text-2xl  font-bold "> Address</div>
+      </div>
+
+      {editToggle === false ? (
+        displayData
+      ) : (
+        <AddressEditForm
+          userData={userData as Iuser}
+          setEditToggle={setEditToggle}
+        />
+      )}
     </div>
   );
 }
